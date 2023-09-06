c53ae443868b26f558dbbae73f8acb2b095f1714
Sachin Iyer <sachinjiyer@gmail.com>
2023-09-05 18:07:05 -0700

    weights file management done

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,6 +1,14 @@
-use axum::{response::Html, routing::get, Json, Router};
+use axum::{
+    http::StatusCode,
+    response::Html,
+    routing::{get, post},
+    Json, Router,
+};
+use dotenv::dotenv;
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
+use std::fs::File;
+use std::io::Write;
 // use model;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -10,10 +18,15 @@ struct Weights {
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+    if std::env::var("WEIGHTS").is_err() {
+        return;
+    }
     let app = Router::new()
         .route("/", get(handler))
         .route("/refresh", get(refresh))
-        .route("/weights", get(weights));
+        .route("/weights", get(weights_get))
+        .route("/weights", post(weights_post));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
@@ -29,11 +42,15 @@ async fn refresh() -> Html<&'static str> {
     Html("<h1>started weights refresh with mnist</h1>")
 }
 
-async fn weights() -> Json<Value> {
-    Json(json!(Weights {
-        weights: (
-            vec![vec![1.0, 2.0], vec![3.0, 4.0]],
-            vec![vec![5.0, 6.0], vec![7.0, 8.0]]
-        )
-    }))
+async fn weights_get() -> Json<Value> {
+    let file = File::open(std::env::var("WEIGHTS").unwrap()).unwrap();
+    let weights: Weights = serde_json::from_reader(file).unwrap();
+    Json(json!({ "weights": weights.weights }))
+}
+
+async fn weights_post(Json(weights): Json<Weights>) -> StatusCode {
+    let mut file = File::create(std::env::var("WEIGHTS").unwrap()).unwrap();
+    file.write_all(serde_json::to_string(&weights).unwrap().as_bytes())
+        .unwrap();
+    StatusCode::OK
 }
