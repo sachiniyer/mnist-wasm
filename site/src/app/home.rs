a57a8e8c725a8656c2158704f4b9ddde45fb78ff
Sachin Iyer <sachinjiyer@gmail.com>
2023-09-06 18:02:32 -0700

    basic train site interaction done

--- site/src/app/home.rs
+++ site/src/app/home.rs
@@ -1,5 +1,7 @@
 use crate::app::Grid;
 use model::Model;
+use wasm_bindgen::JsCast;
+use web_sys::{EventTarget, HtmlInputElement};
 use yew::prelude::*;
 
 #[function_component(Home)]
@@ -7,14 +9,18 @@ pub fn home() -> Html {
     let grid_component_handler = use_state(|| [[false; 28]; 28]);
     let inference_handler = use_state(|| 0);
     let show_grid_handle = use_state(|| false);
-
-    let model = Model::new(
-        (vec![vec![0.0; 728]; 128], vec![vec![0.0; 128]; 10]),
-        (0.0, 0.0),
-    );
+    let input_handle = use_state(|| 0);
+    let loss_handle = use_state(|| 0.0);
+    let model_handle = use_state(|| {
+        Model::new(
+            (vec![vec![0.0; 128]; 728], vec![vec![0.0; 10]; 128]),
+            (0.0, 0.0),
+        )
+    });
 
     let infer_callback = {
         let inference_handler = inference_handler.clone();
+        let model = model_handle.clone();
         Callback::from(move |grid: [[bool; 28]; 28]| {
             let inference_handler = inference_handler.clone();
             let model = model.clone();
@@ -62,6 +68,42 @@ pub fn home() -> Html {
             .join("\n")
     }
 
+    let train_callback = {
+        let input_handle = input_handle.clone();
+        let grid_component_handler = grid_component_handler.clone();
+        let loss_handle = loss_handle.clone();
+        let model_handle = model_handle.clone();
+        Callback::from(move |_| {
+            let input = (*input_handle).clone();
+            let grid = (*grid_component_handler).clone();
+            let loss_handle = loss_handle.clone();
+            let model_handle = model_handle.clone();
+            wasm_bindgen_futures::spawn_local(async move {
+                let mut model = (*model_handle).clone();
+                let grid_train = grid
+                    .iter()
+                    .flatten()
+                    .map(|x| if *x { 1.0 } else { 0.0 })
+                    .collect::<Vec<f64>>();
+                let loss = model.train1d(grid_train, input);
+                web_sys::console::log_1(&format!("loss: {}", loss).into());
+                loss_handle.set(loss);
+                model_handle.set(model);
+            });
+        })
+    };
+
+    let input_callback = {
+        let input_handle = input_handle.clone();
+        Callback::from(move |e: Event| {
+            let target: Option<EventTarget> = e.target();
+            let input = target.and_then(|t| t.dyn_into::<HtmlInputElement>().ok());
+            if let Some(input) = input {
+                input_handle.set(input.value().parse::<i32>().unwrap());
+            }
+        })
+    };
+
     html! {
         <div>
             <h1>{ "MNIST WASM" }</h1>
@@ -76,6 +118,12 @@ pub fn home() -> Html {
                 else { "".to_string() }
             } </div>
             <div>{ format!("Inference: {}", *inference_handler) }</div>
+            <div>
+                <button onclick={ train_callback }>{ "Train Model" }</button>
+                <input onchange={ input_callback }type="number" id="target" name="target" min="0" max="9" />
+                <div>{ format!("Loss {}", *loss_handle) }</div>
+            </div>
+
         </div>
     }
 }
