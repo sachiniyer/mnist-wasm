e29826b16fe289867196faa99af012c98d30e8d6
Sachin Iyer <sachinjiyer@gmail.com>
2023-11-10 00:54:39 -0800

    try to fix tests

--- site/src/hooks/counter.rs
+++ site/src/hooks/counter.rs
@@ -120,53 +120,6 @@ impl PartialEq for UseCounterHandle {
 ///
 /// ```rust
 /// # use yew::prelude::*;
-/// #
-/// use yew_hooks::prelude::*;
-///
-/// #[function_component(Counter)]
-/// fn counter() -> Html {
-///     let counter = use_counter(0);
-///
-///     let onincrease = {
-///         let counter = counter.clone();
-///         Callback::from(move |_| counter.increase())
-///     };
-///     let ondecrease = {
-///         let counter = counter.clone();
-///         Callback::from(move |_| counter.decrease())
-///     };
-///     let onincreaseby = {
-///         let counter = counter.clone();
-///         Callback::from(move |_| counter.increase_by(10))
-///     };
-///     let ondecreaseby = {
-///         let counter = counter.clone();
-///         Callback::from(move |_| counter.decrease_by(10))
-///     };
-///     let onset = {
-///         let counter = counter.clone();
-///         Callback::from(move |_| counter.set(100))
-///     };
-///     let onreset = {
-///         let counter = counter.clone();
-///         Callback::from(move |_| counter.reset())
-///     };
-///
-///     html! {
-///         <div>
-///             <button onclick={onincrease}>{ "Increase" }</button>
-///             <button onclick={ondecrease}>{ "Decrease" }</button>
-///             <button onclick={onincreaseby}>{ "Increase by 10" }</button>
-///             <button onclick={ondecreaseby}>{ "Decrease by 10" }</button>
-///             <button onclick={onset}>{ "Set to 100" }</button>
-///             <button onclick={onreset}>{ "Reset" }</button>
-///             <p>
-///                 <b>{ "Current value: " }</b>
-///                 { *counter }
-///             </p>
-///         </div>
-///     }
-/// }
 /// ```
 #[hook]
 pub fn use_counter(default: i32) -> UseCounterHandle {
--- site/src/hooks/queue.rs
+++ site/src/hooks/queue.rs
@@ -102,57 +102,6 @@ where
 /// # use std::collections::VecDeque;
 /// # use yew::prelude::*;
 /// #
-/// use yew_hooks::prelude::*;
-///
-/// #[function_component(UseQueue)]
-/// fn queue() -> Html {
-///     let queue = use_queue(VecDeque::from(["Mercury", "Venus", "Earth", "Mars"]));
-///
-///     let onset = {
-///         let queue = queue.clone();
-///         Callback::from(move |_| queue.set(VecDeque::from(["Moon", "Earth"])))
-///     };
-///     let onpush_back = {
-///         let queue = queue.clone();
-///         Callback::from(move |_| {
-///             queue.push_back("Jupiter");
-///         })
-///     };
-///     let onpop_front = {
-///         let queue = queue.clone();
-///         Callback::from(move |_| {
-///             let _ = queue.pop_front();
-///         })
-///     };
-///     let onretain = {
-///         let queue = queue.clone();
-///         Callback::from(move |_| queue.retain(|v| v.contains('a')))
-///     };
-///     let onclear = {
-///         let queue = queue.clone();
-///         Callback::from(move |_| queue.clear())
-///     };
-///
-///     html! {
-///         <div>
-///             <button onclick={onset}>{ "Set" }</button>
-///             <button onclick={onpush_back}>{ "Push back" }</button>
-///             <button onclick={onpop_front}>{ "Pop front" }</button>
-///             <button onclick={onretain}>{ "Retain" }</button>
-///             <button onclick={onclear}>{ "Clear all" }</button>
-///             <p>
-///                 <b>{ "Current value: " }</b>
-///             </p>
-///             {
-///                 for queue.current().iter().map(|v| {
-///                     html! {
-///                         <p><b>{ v }</b></p>
-///                     }
-///                 })
-///             }
-///         </div>
-///     }
-/// }
 /// ```
 #[hook]
 pub fn use_queue<T>(initial_value: VecDeque<T>) -> UseQueueHandle<T>
